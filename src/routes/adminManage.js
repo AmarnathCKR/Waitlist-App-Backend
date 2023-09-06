5f5eedb4a0ce3619895f12a2120ef00931a5f7e4
AmarnathCKR <ckramarnath@gmail.com>
2023-09-06 22:59:41 +0530

    Final commit

--- src/routes/adminManage.js
+++ src/routes/adminManage.js
@@ -17,7 +17,6 @@ router.get("/all", async (req, res) => {
 router.post("/update", async (req, res) => {
     const { newPosition, userId } = req.body;
     try {
-        // Find the user to move
         const userToMove = await User.findById(userId);
 
         if (!userToMove) {
@@ -25,26 +24,17 @@ router.post("/update", async (req, res) => {
             return res.status(500).send({ error: "invalid email or password" });
         }
 
-        // Check if the new position is valid
         if (newPosition <= 0) {
             console.error("Invalid position");
             return res.status(500).send({ error: "invalid email or password" });
         }
 
-        
-
-        // Update the user's position
-        
-        
-        // Update positions of other users accordingly
         if (newPosition > userToMove.position) {
-            // Move the user down (increase positions of users below)
             await User.updateMany(
                 { position: { $gt: userToMove.position, $lte: newPosition } },
                 { $inc: { position: -1 } }
             );
         } else if (newPosition < userToMove.position) {
-            // Move the user up (decrease positions of users above)
             await User.updateMany(
                 { position: { $lt: userToMove.position, $gte: newPosition } },
                 { $inc: { position: 1 } }
@@ -55,12 +45,10 @@ router.post("/update", async (req, res) => {
         await userToMove.save();
         console.log(`User with ID ${userId} moved to position ${newPosition}`);
 
-        // Replace this with your email sending logic
         console.log(`Sending notification to user with ID ${userId}`);
 
         res.send(userId);
 
-        // Your email sending code here
 
     } catch (error) {
         console.error("Error moving user position:", error);
@@ -68,7 +56,14 @@ router.post("/update", async (req, res) => {
     }
 })
 
-
+router.get("/delete", async (req,res)=>{
+    await User.findByIdAndDelete({_id:req.query.refId})
+    .then((result)=>{
+        res.send(result);
+    }).catch((err)=>{
+        return res.status(500).send({error : "Server error"});
+    })
+})
 
 
 
